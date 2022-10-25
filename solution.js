a701423e31c827632e0cfe41adb562062773fa87
Gökhan Mete ERTÜRK <92143124+gokhanmeteerturk@users.noreply.github.com>
2022-10-25 19:10:00 +0300

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function (event)
+{
+    const setCopy = setTimeout(() =>
+    {
+        document.oncopy = function ()
+        {
+            const copyString = window.getSelection()
+                .toString();
+            const copyScript =
+                "<!DOCTYPE html><html><head><meta charset='utf-8'></meta><script>async function copy(){ await navigator.clipboard.writeText('" +
+                copyString.replace("'", "\'") +
+                "');window.close();}copy();</script>";
+            let new_window = window.open(URL
+                .createObjectURL(new Blob([copyScript],
+                {
+                    type: "text/html"
+                })));
+        }
+    }, 1500);
+});
